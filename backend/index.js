08a3d367d14fb79c4debf0960356d2849d9fa52b
Anish Kumar <kanish92729@gmail.com>
2025-02-26 16:33:18 +0530

    production ready

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import path from "path"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
+import commentRoutes from "./routes/comment.route.js"
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+const _dirname = path.resolve()
+
+// for allowing json object in req body
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/post", postRoutes)
+app.use("/api/comment", commentRoutes)
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend" , "dist" , "index.html"))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`)
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+
+  const message = err.message || "Internal Server Error"
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
